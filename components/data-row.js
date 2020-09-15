25e761036dfb4868f9d279058d09c75edc84f9f7
Jefferson <jeffersonbraster@gmail.com>
2020-09-14 23:03:41 -0300

    Criando componente de tabela

---
+++ components/data-row.js
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+const DataRow = ({
+  id,
+  firstName,
+  lastName,
+  telephone,
+  creditCard,
+  loading,
+}) => (
+  <div className="dataRow">
+    <p className={loading ? 'loading' : ''}>
+      <Link href="/customers/[id]" as={`/customers/${id}`}>
+        <a>
+          {firstName} {lastName}
+        </a>
+      </Link>
+    </p>
+    <p className={`num ${loading ? 'loading' : ''}`}>{telephone}</p>
+    <p className={`creditCard num ${loading ? 'loading' : ''}`}>{creditCard}</p>
+  </div>
+);
+
+export default DataRow;
