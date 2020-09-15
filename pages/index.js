8c2a72c954a88f5bc4e478655167bdfea2161ba7
Jefferson <jeffersonbraster@gmail.com>
2020-09-14 22:56:10 -0300

    Criando pagina de listagem

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,48 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import useSWR from 'swr';
+import Layout from '../components/layout';
+import DataRow from '../components/data-row';
 
-export default function Home() {
+const fetcher = (url) => fetch(url).then((r) => r.json);
+
+const Home = () => {
+
+  const {data, error} = useSWR('api/customers', fetcher);
+
+  if(error) return <div>Erro ao carregar lista</div>;
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Layout>
+      <h1>Next Fauna Crud</h1>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <div className="table">
+        <h2>Dados do cliente</h2>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className="headerRow">
+          <h4>name</h4>
+          <h4>telefone</h4>
+          <h4 className="creditCard">Cartão de crédito</h4>
         </div>
-      </main>
+      </div>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+      {data ? (
+        data.map((d) =>(
+          <DataRow
+          key={d.ref['@ref'].id}
+          id={d.ref['@ref'].id}
+          firstName={d.data.firstName}
+          lastName={d.data.lastName}
+          telephone={d.data.telephone}
+          creditCard={d.data.creditCard.number} />
+        ))
+      ) : (
+        <>
+          <DataRow loading />
+          <DataRow loading />
+          <DataRow loading />
+        </>
+      )}
+   </Layout>
+  );
 }
+
+export default Home;
